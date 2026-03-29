ab3b12f185c9c9cd3da2486c0f9d37105a7045f8
okeik <willylucy050@gmail.com>
2026-03-29 16:00:19 +0800

    unit test new

--- UnitTestHW/Fizz Buzz/Class/test_fizzbuzz.py
+++ UnitTestHW/Fizz Buzz/Class/test_fizzbuzz.py
@@ -10,13 +10,13 @@ class TestSolution:
         assert solution.singleFizzBuzz(3) == "Fizz"
     
     def test_5(self, solution):
-        ...
+        assert solution.singleFizzBuzz(5) == "Buzz"
     
     def test_15(self, solution):
-        ...
+        assert solution.singleFizzBuzz(15) == "FizzBuzz"
     
     def test_2(self, solution):
-        ...
+        assert solution.singleFizzBuzz(2) == "2"
 
     def test_main_14(self, solution):
         assert solution.<add correct function>(14) == ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14"]
