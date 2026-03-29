9d8f88835e6f921782ac8643e29ae37c1239b764
okeik <willylucy050@gmail.com>
2026-03-29 15:10:45 +0800

    Polished code

--- CleanCodeHW/calculate_pi.py
+++ CleanCodeHW/calculate_pi.py
@@ -11,12 +11,12 @@ inside_circle = 0
 
 # Randomly generate points and count those inside the circle
 for _ in range(NUM_POINTS):
-    x = random.uniform(-RADIUS, RADIUS)
-    y = random.uniform(-1, 1)
-    if x**SQUARE_EXPONENT + y**2 <= RADIUS**SQUARE_EXPONENT:
-        inside_circle += POINT_COUNT_INCREMENT
+    x = random.uniform(-RADIUS, RADIUS)
+    y = random.uniform(-RADIUS, RADIUS)
+    if x**SQUARE_EXPONENT + y**SQUARE_EXPONENT <= RADIUS**SQUARE_EXPONENT:
+        inside_circle += POINT_COUNT_INCREMENT
 
 # Estimate pi based on the number of points inside the circle
-pi_neapple = (inside_circle / NUM_POINTS) * AREA_FACTOR
+estimated_pi = (inside_circle / NUM_POINTS) * AREA_FACTOR
 
-print(f"Estimated value of pi is: {pi_neapple}")
+print(f"Estimated value of pi is: {estimated_pi }")
